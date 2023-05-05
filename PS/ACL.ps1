781419e5ca330d42514456023430f748b31806a9
Kaluva Sailesh <87260152+saileshkaluva@users.noreply.github.com>
2023-05-05 19:05:44 +0530

    PS

---
+++ PS/ACL.ps1
@@ -0,0 +1,19 @@
+$SharePath = "\\interlock0001\recovery\fastcopy\HR Management"
+
+$FolderPath = Get-ChildItem -Directory -Path $SharePath -Recurse
+
+$Output = @()
+
+ForEach ($Folder in $FolderPath)
+
+{
+
+$Acl = Get-Acl -Path $Folder.FullName
+
+$Ar = New-Object System.Security.AccessControl.FileSystemAccessRule ("MNSUK\IT SRV Prod Admin","FullControl","Allow")
+
+$Acl.SetAccessRule($Ar)
+
+Set-Acl -Path $Folder.FullName $Acl
+
+}
